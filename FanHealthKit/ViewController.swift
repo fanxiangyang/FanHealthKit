0dde20c34d99d0ae42549535a8ebabe04bba11fa
fanxiangyang <fanxiangyang_heda@163.com>
2015-11-26 14:40:26 +0800

    Initial Commit

---
+++ FanHealthKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanHealthKit
+//
+//  Created by 向阳凡 on 15/11/26.
+//  Copyright © 2015年 向阳凡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
